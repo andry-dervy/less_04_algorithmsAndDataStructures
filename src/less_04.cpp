cd578930e51a7e58d196b8194d769914b28e0f17
Andry Antonenko <dervy@mail.ru>
2021-07-12 19:44:34 +0300

    Добавлены вторая и третья задачи

--- src/less_04.cpp
+++ src/less_04.cpp
@@ -10,7 +10,7 @@
 #include <cmath>
 
 using namespace std;
-
+//----------------------------------------------------------------------------
 void decToBin(uint d)
 {
   if(d != 0)
@@ -24,7 +24,54 @@ void decToBin(uint d)
     return;
   }
 }
+//----------------------------------------------------------------------------
+int pow_recursive_v01(int a, int b)
+{
+  if(b != 0)
+  {
+    return a * pow_recursive_v01(a, --b);
+  }
+  else
+    return 1;
+}
 
+int pow_recursive_v02(int a, int b)
+{
+  if(b != 0)
+  {
+    if(b%2 == 0)
+    {
+      return pow_recursive_v02(a*a, b/2);
+    }
+    else
+    {
+      return a * pow_recursive_v02(a, --b);
+    }
+  }
+  else
+    return 1;
+}
+//----------------------------------------------------------------------------
+#define xSize  8
+#define ySize  8
+int routes(int* arr, int x, int y)
+{
+  if( (x == 0 && y == 0))
+  {
+    return 0;
+  }
+  else if(*(arr + x*ySize + y) == 1)
+  {
+    return 0;
+  }
+  else if( x == 0 || y == 0 )
+  {
+    return 1;
+  }
+  else
+    return routes(arr, x, y - 1) + routes(arr, x - 1, y);
+}
+//----------------------------------------------------------------------------
 int main(int argc, const char** argv)
 {
 
@@ -33,11 +80,13 @@ int main(int argc, const char** argv)
   используя рекурсию.
 */
 //*
+  printf("Задача 1\n");
   printf("Введите положительное целое число: ");
   uint d = 0;
   scanf("%d",&d);
   printf("0b");
   decToBin(d);
+  printf("\n");
 //*/
 /*
   2. Реализовать функцию возведения числа [a] в степень [b] двумя способами:
@@ -49,6 +98,17 @@ int main(int argc, const char** argv)
   а показатель уменьшается на единицу)
 */
 
+//*
+  printf("\nЗадача 2\n");
+  printf("Введите целое число a: ");
+  int a = 0,b = 0;
+  scanf("%d",&a);
+  printf("Введите целое число b: ");
+  scanf("%d",&b);
+
+  printf("Результат функции pow_recursive_v01: %d\n",pow_recursive_v01(a,b));
+  printf("Результат функции pow_recursive_v02: %d\n",pow_recursive_v02(a,b));
+//*/
 
 /*
   3. Реализовать нахождение количества маршрутов шахматного короля с препятствиями
@@ -57,6 +117,28 @@ int main(int argc, const char** argv)
   Результатом работы должен стать один файл с кодом на языке С, содержащий функцию main и четыре функции, соответствующие каждому из заданий.
 //*/
 
+//*
+  printf("\nЗадача 3\n");
+  int chessArray[ySize][xSize] = {
+    {0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0},
+    {0,0,0,0,1,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,1,0,1,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0}
+  };
+
+  for(int x = 0; x < xSize; x++)
+  {
+    for(int y = 0; y < ySize; y++)
+    {
+      printf("%5d",routes(&chessArray[0][0],x,y));
+    }
+    printf("\n");
+  }
+//*/
 
   return 0;
 }
